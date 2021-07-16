b300698a53d9674e9013e094d39779578742dd95
Your Name <you@example.com>
2021-07-16 14:04:56 -0400

    migration tables created and running

--- data/migrations/20210716173804_sprint4-2.js
+++ data/migrations/20210716173804_sprint4-2.js
@@ -1,8 +1,54 @@
 
 exports.up = function (knex) {
+    return knex.schema.createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 128).notNullable();
+        table.string('project_description', 255).nullable();
+        table.boolean('project_completed').nullable().defaultTo(false);
+    })
+        .createTable('resources', table => {
+            table.increments('resource_id');
+            table.string('resource_name', 128).notNullable().unique()
+            table.string('resource_description').nullable()
+        })
 
+        .createTable('tasks', table => {
+            table.increments('task_id');
+            table.string('task_description', 255).notNullable();
+            table.string('task_notes', 255).nullable();
+            table.boolean('task_completed').nullable().defaultTo(false);
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
+
+        .createTable('project_resources', table => {
+            table.increments('pr_id');
+            table.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+            table.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('project_id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
 };
